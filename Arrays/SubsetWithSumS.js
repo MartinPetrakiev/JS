1e831ede6f6e3e496bbe531076ff7286e80366b3
martinmp <martomsl@gmail.com>
2024-01-09 15:06:04 +0200

    alternative solution task 8

--- Arrays/SubsetWithSumS.js
+++ Arrays/SubsetWithSumS.js
@@ -18,7 +18,46 @@ function findSubsetWithSumEqualToS (array, S) {
     return dynamicArray[array.length][S] ? 'yes' : 'no';
 }
 
+//testing alternative
+function findSubsetWithSumEqualToS_2(inputArray, S) {
+    if (S === 0) {
+        return 'yes';
+    }
+
+    if (inputArray.length === 0 && S !== 0) {
+        return 'no';
+    }
+
+    if (inputArray[inputArray.length - 1] <= S) {
+        if (findSubsetWithSumEqualToS_2(inputArray.slice(0, inputArray.length - 1), S - inputArray[inputArray.length - 1])) {
+            return 'yes';
+        }
+    }
+
+    return findSubsetWithSumEqualToS_2(inputArray.slice(0, inputArray.length - 1), S);
+}
+
+//gpt alternative - (slow performance)
+function findSubsetWithSumEqualToS_3(arr, S) {
+    const n = arr.length;
+    
+    for (let mask = 0; mask < (1 << n); mask++) {
+        let currentSum = 0;
+        for (let j = 0; j < n; j++) {
+            if (mask & (1 << j)) {
+                currentSum += arr[j];
+            }
+        }
+        if (currentSum === S) {
+            return 'yes';
+        }
+    }
+    return 'no';
+}
+
 console.log(findSubsetWithSumEqualToS([2, 1, 2, 4, 3, 5, 2, 6], 14))
+
+console.log(findSubsetWithSumEqualToS_2([2, 1, 2, 4, 3, 5, 2, 6], 14))
 
 /*
 08. Subset with sum S
