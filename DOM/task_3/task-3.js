3d89e1fc6eaa0ba65e96882826d1d3adaa52eaa2
martinmp <martomsl@gmail.com>
2024-01-15 12:36:58 +0200

    finished solution task 3

--- DOM/task_3/task-3.js
+++ DOM/task_3/task-3.js
@@ -1,21 +1,43 @@
 function solve() {
+    function toggleShowHide(e) {
+        var topmostContentElement = e.target.previousElementSibling;
+        if (!topmostContentElement || !topmostContentElement.classList.contains("content")) {
+            return;
+        }
+        if (topmostContentElement.classList.contains("hidden")) {
+            topmostContentElement.classList.remove("hidden");
+            topmostContentElement.style.display = "block";
+            e.target.textContent = "hide";
+        }
+        else {
+            topmostContentElement.classList.add("hidden");
+            topmostContentElement.style.display = "none";
+            e.target.textContent = "show";
+        }
+    }
     return function (selector) {
         if (!selector) {
-            throw new Error('Invalid selector.');
+            throw new Error("Invalid selector.");
         }
-        var parentElemet;
+        var parentElement;
         if (typeof selector === "string") {
-            parentElemet = document.getElementById(selector);
-            if (!parentElemet) {
+            parentElement = document.getElementById(selector);
+            if (!parentElement) {
                 throw new Error("Invalid id or missing element.");
             }
         }
         else if (selector instanceof HTMLElement) {
-            parentElemet = selector;
+            parentElement = selector;
         }
         else {
             throw new Error("Invalid element type.");
         }
+        var childElementsArray = Array.from(parentElement.children);
+        var buttonElements = childElementsArray.filter(function (element) { return element.classList.contains("button") ? element : null; });
+        buttonElements.forEach(function (element) {
+            element.textContent = "hide";
+            element.addEventListener("click", toggleShowHide);
+        });
     };
 }
 ;
